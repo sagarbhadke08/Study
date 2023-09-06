6df3d47ecc2cea6df7118675ab49123063bb8b6a
Sagar Bhadke <94558820+sagarbhadke08@users.noreply.github.com>
2023-09-06 23:54:57 +0530

    Update twoSum.java

--- twoSum.java
+++ twoSum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       
+       //solve tommorow
        for(int i=0; i<nums.length;i++){
            for(int j=i+1; j<nums.length;j++){
 
