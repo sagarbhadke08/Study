e348176d13b07d740a7b6188329d1e6d9a77e177
Sagar Bhadke <94558820+sagarbhadke08@users.noreply.github.com>
2023-10-04 00:41:59 +0000

    Palidrome Array or Not

---
+++ palidromeOrNot.java
@@ -0,0 +1,18 @@
+class palidromeOrNot{
+    public static void main(String[] args){
+        System.out.println("SDE at Microsoft");
+
+        int[] arr;
+        arr = new int[5];
+
+        arr[0]= 10;
+        arr[1]=12;
+        arr[2]=15;
+        arr[3]=45;
+        arr[4]=23;
+
+       for(int i: arr){
+        System.out.println(arr[i]);
+       }
+    }
+}
