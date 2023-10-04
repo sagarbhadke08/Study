047553d75961bc7870db2b571e426610816bbb6f
Sagar Bhadke <94558820+sagarbhadke08@users.noreply.github.com>
2023-10-04 00:47:33 +0000

    updated

--- palidromeOrNot.java
+++ palidromeOrNot.java
@@ -1,4 +1,6 @@
 class palidromeOrNot{
+   
+
     public static void main(String[] args){
         System.out.println("SDE at Microsoft");
 
@@ -10,6 +12,7 @@ class palidromeOrNot{
         arr[2]=15;
         arr[3]=45;
         arr[4]=23;
+    
 
        for(int i: arr){
         System.out.println(arr[i]);
